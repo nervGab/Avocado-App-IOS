3f6457cc2dd2aa9617438de0cf5028a24093eb18
Juan Manuel Pereira Sanchez <jmps1389@gmail.com>
2022-02-15 12:50:56 +0100

    [WIP] - Avocado App 1st commit -

---
+++ AvocadoApp/AvocadoApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  AvocadoApp
+//
+//  Created by Juan Manuel Pereira Sanchez on 15/2/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
